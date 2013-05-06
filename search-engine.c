0757cd5c60ffee264aacccb9033a41db1e0e41ad
spamblebridge <bambridge.peterson@gmail.com>
2013-05-06 15:17:22 -0500

    started the scanner and indexer threads

--- search-engine.c
+++ search-engine.c
@@ -3,11 +3,31 @@
 #include <string.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <assert.h>
 #include <semaphore.h>
 
 #include "index.h"
 
 #define DEBUG
+// ADDED 
+typedef struct bounded_buffer_s {
+	char ** buffer;
+	int fill;
+	int use;
+	int count;
+	int done;
+	int size; 
+} Bounded_Buffer; 
+
+// ADDED
+typedef Bounded_Buffer * bounded_buffer_t; 
+// ADDED
+typedef struct mutex_cond {
+	pthread_mutex_t bb_mutex;
+	pthread_cond_t empty;
+	pthread_cond_t full; 
+} Mutex_Cond; 
+Mutex_Cond mutex_cond; 
 
 typedef struct tag_args {
     int num_indexer_threads;
@@ -18,12 +38,17 @@ Args args;
 typedef struct tag_info {
     FILE *file_list;
     // TODO : put bounded buffer in here?
+	// ADDED: Bounded_Buffer pointer 
+	bounded_buffer_t bbp; 
 } Info;
 Info info;
 
 
 void parseArgs(int argc, char *argv[]);
-void startScanner();
+void startScanner(Info info);
+// ADDED 
+void initBoundedBuffer();
+void initMutexStruct();
 void startIndexer();
 void startSearch();
 void cleanup();
@@ -33,8 +58,13 @@ void cleanup();
 // Entry point ----------------------------------------------------------------
 // ----------------------------------------------------------------------------
 int main(int argc, char *argv[]) {
+	// ADDED 
     parseArgs(argc, argv);
-    startScanner();
+
+	// ADDED
+	initBoundedBuffer();
+	initMutexStruct(); 
+    startScanner(info);
     startIndexer();
     startSearch();
     cleanup();
@@ -48,6 +78,33 @@ void usage() {
     exit(1);
 }
 
+//-----------------------------------------------------------------------------
+void initBoundedBuffer() {
+	char ** buffer;
+	int i;
+
+	buffer = malloc(sizeof(char *) * args.num_indexer_threads);
+	
+	for(i = 0; i < args.num_indexer_threads; i += 1) {
+		buffer[i] = malloc(sizeof(char) * MAXPATH);
+		memset(buffer[i], 0, MAXPATH);
+	} 
+
+	Bounded_Buffer bb = {buffer, 0, 0, 0, 0, args.num_indexer_threads};
+	info.bbp = &bb;
+}
+
+//-----------------------------------------------------------------------------
+void initMutexStruct() {
+	int rc; 
+	memset(&mutex_cond, 0, sizeof(Mutex_Cond));
+	rc = pthread_mutex_init(&mutex_cond.bb_mutex, NULL);
+	assert(rc == 0);
+	rc = pthread_cond_init(&mutex_cond.empty, NULL);
+	assert(rc == 0);
+	rc = pthread_cond_init(&mutex_cond.full, NULL); 
+	assert(rc == 0);
+}
 // ----------------------------------------------------------------------------
 void parseArgs(int argc, char *argv[]) {
     if (argc != 3) {
@@ -81,13 +138,50 @@ void parseArgs(int argc, char *argv[]) {
 }
 
 // ----------------------------------------------------------------------------
+
+void add_to_buffer(char * filename) {
+	printf("in add_to_buffer and filename = %s\n", filename);
+	info.bbp->buffer[info.bbp->fill] = filename;
+	info.bbp->fill = (info.bbp->fill) % info.bbp->size;
+	info.bbp->count++;
+}
+
 void* scannerWorker(void *data) {
     // TODO : scan files from file list
+	Info * ip = (Info *) data; 
+	char * line;
+	int i;
+	
+	if((line = malloc(MAXPATH * sizeof(char))) == NULL) {
+		printf("Error allocating memory in scannerWorker\n");
+		exit(1);
+	}
+
+	while ((line = fgets(line, MAXPATH, ip->file_list)) != NULL) {
+		if (line[strlen(line) - 1] == '\n')
+			line[strlen(line) - 1] = 0;
+
+		pthread_mutex_lock(&mutex_cond.bb_mutex);
+		while (ip->bbp->count == ip->bbp->size) {
+			pthread_cond_wait(&mutex_cond.empty, &mutex_cond.bb_mutex); 
+		}
+
+		add_to_buffer(line);
+		pthread_cond_signal(&mutex_cond.full);
+		pthread_mutex_unlock(&mutex_cond.bb_mutex); 
+	}
+	
     return NULL;
 }
 
-void startScanner() {
+void startScanner(Info info) {
     // TODO : start scanner thread
+	printf("call scannerWorder\n");
+	pthread_t scanner_thread;
+	int rc;
+	rc = pthread_create(&scanner_thread, NULL, scannerWorker, &info);
+	assert(rc == 0);
+	pthread_exit(NULL);
 }
 
 // ----------------------------------------------------------------------------
@@ -113,6 +207,20 @@ void* indexerWorker(void *data) {
 
 void startIndexer() {
     // TODO : start indexer thread
+
+	// HOW TO DO THIS?
+	// Give it a shot here
+	int i; 
+	int rc; 
+	pthread_t * indexer_threads;
+	indexer_threads = (pthread_t *) calloc(args->num_indexer_threads * sizeof(pthread_t));
+
+	for (i = 0; i < args->num_indexer_threads; i += 1) {
+		int rc = pthread_create(&indexer_threads[i], NULL, indexerWorker, NULL);
+		assert(rc == 0);
+	}
+	// Do we have to call pthread_join???
+	// Or pthread_exit(NULL)???	
 }
 
 // ----------------------------------------------------------------------------
